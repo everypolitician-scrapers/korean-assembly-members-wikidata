ecd7362e3579e1023fdb427a66e21f7f853b692f
Tony Bowden <tony@tmtm.com>
2015-12-21 17:30:55 +0000

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,43 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'rest-client'
+require 'scraperwiki'
+require 'wikidata/fetcher'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+
+def noko_for(url)
+  Nokogiri::HTML(open(URI.escape(URI.unescape(url))).read) 
+end
+
+def wikinames_from(url)
+  noko = noko_for(url)
+  noko.xpath("//h2/span[.='가나다순 보기']//preceding::*").remove
+  noko.xpath("//h2/span[.='의석 변동']//following::*").remove
+  names = noko.xpath('//p//a[not(@class="new")]/@title').map(&:text).uniq
+  raise "No names found in #{url}" if names.count.zero?
+  return names
+end
+
+def fetch_info(names)
+  WikiData.ids_from_pages('ko', names).each do |name, id|
+    data = WikiData::Fetcher.new(id: id).data('ko') rescue nil
+    unless data
+      warn "No data for #{p}"
+      next
+    end
+    data[:original_wikiname] = name
+    warn data
+    ScraperWiki.save_sqlite([:id], data)
+  end
+end
+
+fetch_info wikinames_from('https://ko.wikipedia.org/wiki/%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD_%EC%A0%9C19%EB%8C%80_%EA%B5%AD%ED%9A%8C%EC%9D%98%EC%9B%90_%EB%AA%A9%EB%A1%9D')
+
+warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+
