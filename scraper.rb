70784d3739d2ff3be902b801111570b2d5ab7219
Tony Bowden <tony@tmtm.com>
2016-10-14 20:13:02 +0100

    Also get members by Category

--- scraper.rb
+++ scraper.rb
@@ -4,18 +4,22 @@
 require 'wikidata/fetcher'
 require 'pry'
 
-names_19 = EveryPolitician::Wikidata.wikipedia_xpath(
+
+site_19 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://ko.wikipedia.org/wiki/대한민국_제19대_국회의원_목록',
   after: "//h2/span[.='가나다순 보기']",
   before: "//h2/span[.='의석 변동']",
   xpath: '//p//a[not(@class="new")]/@title',
 )
+category_19 = WikiData::Category.new('분류:대한민국의 제19대 국회의원', 'ko').member_titles
 
-names_20 = EveryPolitician::Wikidata.wikipedia_xpath(
+site_20 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://ko.wikipedia.org/wiki/대한민국_제20대_국회의원_목록',
   after: "//h2/span[.='가나다순 보기']",
   before: "//h2/span[.='의석 변동']",
   xpath: '//p//a[not(@class="new")]/@title',
 )
+category_20 = WikiData::Category.new('분류:대한민국의 제20대 국회의원', 'ko').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { ko: names_19 | names_20 }, output: false)
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { ko: site_19 | category_19 | site_20 | category_20 })
